0299b60f35bd3fd24ba75d1645b27774ea0ae030
Xaelitry <14274548+Xaelitry@users.noreply.github.com>
2021-07-26 22:40:35 +1000

    Add files via upload

---
+++ PassGen_Ver1.py
@@ -0,0 +1,59 @@
+import random
+import os
+import time
+from pathlib import Path
+
+file = Path('Output.txt')
+fname = 'Output.txt'
+
+os.system('title Xaelitry\'s Secure Password Generator')
+
+def randompass(length):
+    return "".join([random.choice('!@#$%^&*_+-=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') for x in range(int(length))])
+
+if __name__ == "__main__":
+
+    while True:
+
+        p = input("What is the password for? ")
+        mail = input("Enter email attached to account: ")
+        lenV = input("Set length for password: ")
+
+        if lenV.isdigit():
+            pw = randompass(int(lenV))
+            v = ""
+            v = v.join(p.title()+":\nEmail: "+mail+"\nPassword: "+pw)
+            if file.is_file():
+                with open(fname, 'a') as f:
+                    f.write("\n\n"+v)
+                    f.close()
+                    print('Generated password for '+p.title()+'\nPassword: '+pw+'\n\nPassword has also been written to file in '+fname+'.\n\n')
+            else:
+                with open(fname, 'w+') as f:
+                    f.write(v)
+                    f.close()
+                    print('Generated password for '+p.title()+'\nPassword: '+pw+'\n\nPassword has also been written to file in '+fname+'.\n\n')
+
+            cont = input('Do you want to continue? [Y/N] ')
+            yes = ['y', 'Y', 'yes', 'Yes', 'YES']
+            no = ['n', 'N', 'no', 'No', 'NO']
+            if cont in yes:
+                print('Moving onto next job.')
+                time.sleep(3)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                continue
+            elif cont in no:
+                print('Closing application.')
+                time.sleep(3)
+                exit()
+            else:
+                print('An error occurred, input was not recognized.')
+                time.sleep(3)
+                os.system(cls)
+                continue
+            
+        else:
+            print("Input for length was not a number, try again.")
+            time.sleep(3)
+            os.system("cls")
+            continue
